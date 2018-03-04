650efcb9e88f1a84b6ab8c311c1d9b922650cf37
Micah Pearce <micah.r.pearce@gmail.com>
2018-03-03 18:19:26 -0600

    Fix email spelling error

--- ecommerce/settings/local_micah.py
+++ ecommerce/settings/local_micah.py
@@ -25,7 +25,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'goldLeader216e@gmail.com' 
+EMAIL_HOST_USER = 'goldLeader216@gmail.com' 
 EMAIL_HOST_PASSWORD = 'django216'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -26,7 +26,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['.herokuapp.com']
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'goldLeader216e@gmail.com' 
+EMAIL_HOST_USER = 'goldLeader216@gmail.com' 
 EMAIL_HOST_PASSWORD = 'django216'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
